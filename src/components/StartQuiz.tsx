f810cf38e8d47d5e11808664a3cd5acf8e5ba4b3
Aleksi <liukkonenaleksi@gmail.com>
2024-08-21 13:07:32 +0300

    Add question next logic

--- src/components/StartQuiz.tsx
+++ src/components/StartQuiz.tsx
@@ -1,21 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { quizData } from '../data/quizdata';
 
 const Quiz = () => {
-  console.log('Quiz Data:', quizData);
-  
-  return (
+
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
+    const [isAnswered, setIsAnswered] = useState(false);
+    const [isCorrect, setIsCorrect] = useState(false);
+
+  const currentQuestion = quizData[currentQuestionIndex];
+
+  const handleAnswerSelection = (answer: string) => {
+    setSelectedAnswer(answer);
+  };
+
+  const handleSubmit = () => {
+    setIsCorrect(selectedAnswer === currentQuestion.correctAnswer);
+    setIsAnswered(true);
+  };
+
+  const handleNextQuestion = () => {
+    setIsAnswered(false);
+    setSelectedAnswer(null);
+    if (currentQuestionIndex < quizData.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    }
+  };
+
+return (
     <div>
-      {quizData.map((quizItem, index) => (
-        <div key={index}>
-          <p>Question {index + 1}: {quizItem.text}</p>
-          <ul>
-            {quizItem.options.map((option, optIndex) => (
-              <li key={optIndex}>{option}</li>
-            ))}
-          </ul>
+      <h2>{currentQuestion.text}</h2>
+      <ul>
+        {currentQuestion.options.map((option, index) => (
+          <li key={index}>
+            <input
+              type="radio"
+              name={`quiz-option-${currentQuestion.id}`}
+              onChange={() => handleAnswerSelection(option)}
+              checked={selectedAnswer === option}
+            />
+            <label>{option}</label>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleSubmit} disabled={!selectedAnswer}>
+        Submit Answer
+      </button>
+      {isAnswered && (
+        <div>
+          {isCorrect ? <p>Correct!</p> : <p>Incorrect, the correct answer is "{currentQuestion.correctAnswer}".</p>}
+          {currentQuestionIndex < quizData.length - 1 && (
+            <button onClick={handleNextQuestion}>Next Question</button>
+          )}
         </div>
-      ))}
+      )}
     </div>
   );
 };
