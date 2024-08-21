64506a1af7c501cf9c53ea2fb2e24a50a90c32ec
Aleksi <liukkonenaleksi@gmail.com>
2024-08-21 13:44:52 +0300

    Style quiz

--- src/components/StartQuiz.tsx
+++ src/components/StartQuiz.tsx
@@ -3,10 +3,10 @@ import { quizData } from '../data/quizdata';
 
 const Quiz = () => {
 
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
-    const [isAnswered, setIsAnswered] = useState(false);
-    const [isCorrect, setIsCorrect] = useState(false);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
+  const [isAnswered, setIsAnswered] = useState(false);
+  const [isCorrect, setIsCorrect] = useState(false);
 
   const currentQuestion = quizData[currentQuestionIndex];
 
@@ -28,29 +28,35 @@ const Quiz = () => {
   };
 
 return (
-    <div>
-      <h2>{currentQuestion.text}</h2>
-      <ul>
+    <div className="max-w-4xl">
+      <h2 className="text-4xl mb-8">{currentQuestion.text}</h2>
+      <ul className="grid grid-cols-2 gap-4 ">
         {currentQuestion.options.map((option, index) => (
-          <li key={index}>
-            <input
+          <li key={index}> 
+  <label className="block p-4 border bg-white border-gray-300 rounded-lg cursor-pointer hover:bg-gray-100 text-gray-700">
+  <input
               type="radio"
               name={`quiz-option-${currentQuestion.id}`}
               onChange={() => handleAnswerSelection(option)}
               checked={selectedAnswer === option}
+              disabled={isAnswered}
             />
-            <label>{option}</label>
+      <span className='ml-2'>{option}</span>
+  </label>
+       
           </li>
         ))}
       </ul>
-      <button onClick={handleSubmit} disabled={!selectedAnswer}>
-        Submit Answer
-      </button>
+      {!isAnswered && (
+        <button className="mt-8 px-6 py-4 text-lg bg-orange-500 hover:bg-orange-600 text-white font-semibold rounded-full inline-block" onClick={handleSubmit} disabled={!selectedAnswer}>
+          Submit Answer
+        </button> 
+      )}
       {isAnswered && (
         <div>
-          {isCorrect ? <p>Correct!</p> : <p>Incorrect, the correct answer is "{currentQuestion.correctAnswer}".</p>}
+          {isCorrect ? <p className="text-lg mt-4"> Correct!</p> : <p className="text-lg mt-4">Incorrect, the correct answer is "{currentQuestion.correctAnswer}".</p>}
           {currentQuestionIndex < quizData.length - 1 && (
-            <button onClick={handleNextQuestion}>Next Question</button>
+            <button className="mt-8 px-6 py-4 text-lg bg-white hover:bg-orange-600 text-gray-700 font-semibold rounded-full inline-block" onClick={handleNextQuestion}>Next Question</button>
           )}
         </div>
       )}
